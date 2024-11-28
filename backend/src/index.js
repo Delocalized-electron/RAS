a2ae4308f989569056dd89f7dfb862363e4f92d7
Aadil Khatri <aadil.khatri343@gmail.com>
2024-11-28 18:17:36 +0530

    setting up express app and utils

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,14 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+  });
