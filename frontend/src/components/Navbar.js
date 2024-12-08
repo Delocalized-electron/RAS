6406287906dd009d641a8e99e9ddbe113b38ef5a
Aadil Khatri <aadil.khatri343@gmail.com>
2024-12-08 17:31:11 +0530

    Add Navbar component to AppRoutes and update styles for improved layout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="">
+    <div className="fixed bg-black flex justify-between gap-1 py-3 px-10 left-1/2 translate-x-[50%] top-[20px] rounded-full backdrop-blur-md bg-opacity-60 text-white shadow-lg">
       <div></div>
       <ul>
         <li></li>
--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Home = lazy(() => import("../pages/Home"));
 const AddItems = lazy(() => import("../pages/AddItems"));
@@ -9,6 +10,7 @@ const NotFound = lazy(() => import("../pages/NotFound"));
 const AppRoutes = () => {
   return (
     <Router>
+      <Navbar />
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
