2d4d9ef988198d70d18912e6ecc1310a3bc898d1
Aadil Khatri <aadil.khatri343@gmail.com>
2024-12-08 13:11:53 +0530

    Rename Stock component to Stocks for consistency; update export statement

--- frontend/src/pages/Stocks.js
+++ frontend/src/pages/Stocks.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Stock = () => {
-  return <div>Stock</div>;
+const Stocks = () => {
+  return <div>Stocks</div>;
 };
 
-export default Stock;
+export default Stocks;
