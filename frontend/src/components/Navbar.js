e56d26f46ae5ba65e2b5e654c417e2e13c363c40
Aadil Khatri <aadil.khatri343@gmail.com>
2024-12-09 16:46:23 +0530

    experimenting with navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,73 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
+const lisItems = ["Home", "Add items", "Invetory", "Low stock"];
+const listIcons = [];
 const Navbar = () => {
   return (
-    <div className="fixed bg-black flex justify-between gap-1 py-3 px-10 left-1/2 translate-x-[50%] top-[20px] rounded-full backdrop-blur-md bg-opacity-60 text-white shadow-lg">
-      <div></div>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+    <div className="fixed bg-white justify-between gap-1 py-3 px-10 right-1/2 translate-x-[50%] bottom-[20px] rounded-full backdrop-blur-md bg-opacity-30 text-slate shadow-lg z-10 shadow-transparent md:flex hidden">
+      <ul className="flex gap-8 text-base">
+        {/* {lisItems.map((item) => {
+          return (
+            <>
+              <li
+                className="relative text-slate-700 group cursor-pointer   hover:shadow "
+                key={item}
+              >
+                {item}
+                <span className="absolute left-0 bottom-[-5px] w-0 h-1 rounded-xl bg-gradient-to-r from-white to-grey transition-all duration-300 group-hover:w-full"></span>
+              </li>
+            </>
+          );
+        })} */}
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-slate-500 text-white px-3 py-2 rounded-full  "
+                : "text-gray-300 hover:bg-gray-300 hover:text-white px-3 py-2 rounded-full  "
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/addItems"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-slate-500 text-white px-3 py-2 rounded-full  "
+                : "text-gray-300 hover:bg-gray-300 hover:text-white px-3 py-2 rounded-full  "
+            }
+          >
+            Add Items
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/stocks"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-slate-500 text-white px-3 py-2 rounded-full  "
+                : "text-gray-300 hover:bg-gray-300 hover:text-white px-3 py-2 rounded-full  "
+            }
+          >
+            Inventory
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/lowStock"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-slate-500 text-white px-3 py-2 rounded-full  "
+                : "text-gray-300 hover:bg-gray-300 hover:text-white px-3 py-2 rounded-full  "
+            }
+          >
+            Low stock
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Loading from "../pages/Loading";
 
 const Home = lazy(() => import("../pages/Home"));
 const AddItems = lazy(() => import("../pages/AddItems"));
@@ -11,11 +12,12 @@ const AppRoutes = () => {
   return (
     <Router>
       <Navbar />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/addItems" element={<AddItems />} />
           <Route path="/stocks" element={<Stocks />} />
+          <Route path="/loading" element={<Loading />} />
           <Route path="*" element={<NotFound />} /> {/* 404 Page */}
         </Routes>
       </Suspense>
