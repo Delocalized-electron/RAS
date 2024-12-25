7ffb1020e5fd0ac93277507d048d508078fcf14c
Aadil Khatri <aadil.khatri343@gmail.com>
2024-12-25 21:20:25 +0530

    Redirect authenticated users from login page to home

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -1,10 +1,11 @@
 import React, { lazy, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Loading from "../pages/Loading";
 import Login from "../pages/Login";
 import ProtectedRoute from "./ProtectedRoute";
 import Register from "../pages/Register";
 import Temp from "../pages/Temp";
+import { useSelector } from "react-redux";
 
 const Home = lazy(() => import("../pages/Home"));
 const AddItems = lazy(() => import("../pages/AddItems"));
@@ -12,10 +13,14 @@ const Stocks = lazy(() => import("../pages/Stocks"));
 const NotFound = lazy(() => import("../pages/NotFound"));
 
 const AppRoutes = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/" replace /> : <Login />}
+        />
         <Route path="/register" element={<Register />} />
         <Route
           path="/"
